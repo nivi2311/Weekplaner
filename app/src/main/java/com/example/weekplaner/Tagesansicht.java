0ec233b82eb04dfb64eabe7877e7809d4cb57c11
Nivet <Nivetha.Thaventhiran@hotmail.com>
2020-11-12 13:22:26 +0100

    newest gui updates

--- app/src/main/java/com/example/weekplaner/Tagesansicht.java
+++ app/src/main/java/com/example/weekplaner/Tagesansicht.java
@@ -8,6 +8,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Tagesansicht extends AppCompatActivity {
 
@@ -15,5 +17,11 @@ public class Tagesansicht extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tagesansicht);
+        Button button = (Button) findViewById(R.id.createT);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_termin_formular);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/weekplaner/TerminFormular.java
@@ -0,0 +1,14 @@
+package com.example.weekplaner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TerminFormular extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termin_formular);
+    }
+}
